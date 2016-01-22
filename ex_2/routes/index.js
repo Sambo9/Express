06b4198481ee3578a044a659cf3b05e9627a0900
Sambo <losangeles9@gmail.com>
2016-01-22 12:35:43 +0100

    ex_2

--- ex_2/routes/index.js
+++ ex_2/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path    = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/index', function(req, res, next) {
+    fs.readFile('public/index.html', function (err, data) {
+    if (err) throw err;
+    res.set('Content-Type', 'text/html');
+    res.send(data);
+  });
 });
 
 module.exports = router;
